79a5d00ede57e1181f24a53019a1901adf95b37a
SørenHN <sorenherskindnielsen@gmail.com>
2020-09-03 13:41:34 +0200

    Adds circle intersection example to example directory

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import analyser.RelationChecker;
+import ast.Program;
+import executor.Executor;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception
     {
-        TestMain.main();
+        GdslParse parser = new GdslParse();
+        InputStream inputStream = null;
+        String fileName = "examples/circleIntersection.gdsl";
+
+        try{
+            inputStream = new FileInputStream(fileName);
+        }catch(IOException e){
+            System.out.println("Could not find file: " + fileName);
+        }
+
+        Program program = parser.parse(inputStream);
+        Executor executor = new Executor();
+        System.out.println(executor.executeProgram(program, "main"));
+
+        //RelationChecker.checkRelations(program);
     }
 }
